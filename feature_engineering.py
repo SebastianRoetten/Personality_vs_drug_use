6a445233e691c6c919beef9680132d793bf145c0
Laura Dell'Antonio <laura@dellantonio.de>
2020-11-04 17:11:53 +0100

    Script for feature engineering and training model

--- feature_engineering.py
+++ feature_engineering.py
@@ -5,6 +5,7 @@ import numpy as np
 def drop_semer(df:pd.DataFrame)-> pd.DataFrame:
     df = df.drop(df.loc[df["Semer"] != "CL0"].index)
     df.drop('Semer', inplace=True, axis=1)
+    return df 
 
 
 
@@ -18,63 +19,47 @@ def use_non_use(df:pd.DataFrame)-> pd.DataFrame:
         cat = df[element].apply(lambda x: 0 if (x == "CL0" or x == "CL1") else 1)
         df.insert(column_insert, col_name, cat)
         df[col_name] = df[col_name].astype("category")
-
-
-
-def drug_group_1(df:pd.DataFrame)-> pd.DataFrame:
-    df_group1_drugs = df.drop(["Amphet_Cat", "Amyl_Cat", "Benzos_Cat", "Coke_Cat", "Crack_Cat", "Ecstasy_Cat", 'Heroin_Cat', 'Ketamine_Cat', 
-                        'Legalh_Cat', 'LSD_Cat', 'Meth_Cat', 'Shrooms_Cat', 'VSA_Cat', 'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caff', 
-                        'Cannabis', 'Choc', 'Coke', 'Crack', 'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Shrooms',
-                        'Nicotine', 'VSA'], axis=1)
-
-    df_group1_drugs["Alcohol_Cat"] = df_group1_drugs["Alcohol_Cat"].astype(int)
-    df_group1_drugs["Caff_Cat"] = df_group1_drugs["Caff_Cat"].astype(int)
-    df_group1_drugs["Cannabis_Cat"] = df_group1_drugs["Cannabis_Cat"].astype(int)
-    df_group1_drugs["Choc_Cat"] = df_group1_drugs["Choc_Cat"].astype(int)
-    df_group1_drugs["Nicotine_Cat"] = df_group1_drugs["Nicotine_Cat"].astype(int)
-
-
-
-def drug_group_2(df:pd.DataFrame)-> pd.DataFrame:
-    df_group2_drugs = df.drop(["Alcohol_Cat", "Amyl_Cat", "Caff_Cat", "Cannabis_Cat", "Choc_Cat", "Crack_Cat",
-                        "Heroin_Cat", "Ketamine_Cat", "Meth_Cat", "Nicotine_Cat", "VSA_Cat", "Alcohol", "Amphet", "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke", "Crack",
-                        "Ecstasy", "Heroin", "Ketamine", "Legalh", "LSD", "Meth", "Shrooms", "Nicotine", "VSA"], axis=1)
     
-    df_group2_drugs["Amphet_Cat"] = df_group2_drugs["Amphet_Cat"].astype(int)
-    df_group2_drugs["Benzos_Cat"] = df_group2_drugs["Benzos_Cat"].astype(int)
-    df_group2_drugs["Coke_Cat"] = df_group2_drugs["Coke_Cat"].astype(int)
-    df_group2_drugs["Ecstasy_Cat"] = df_group2_drugs["Ecstasy_Cat"].astype(int)
-    df_group2_drugs["Legalh_Cat"] = df_group2_drugs["Legalh_Cat"].astype(int)
-    df_group2_drugs["LSD_Cat"] = df_group2_drugs["LSD_Cat"].astype(int)
-    df_group2_drugs["Shrooms_Cat"] = df_group2_drugs["Shrooms_Cat"].astype(int)   
+    return df
+
+def make_int(df):
+    df["Alcohol_Cat"] = df["Alcohol_Cat"].astype(int)
+    df["Caff_Cat"] = df["Caff_Cat"].astype(int)
+    df["Cannabis_Cat"] = df["Cannabis_Cat"].astype(int)
+    df["Choc_Cat"] = df["Choc_Cat"].astype(int)
+    df["Nicotine_Cat"] = df["Nicotine_Cat"].astype(int)
+
+    df["Amphet_Cat"] = df["Amphet_Cat"].astype(int)
+    df["Benzos_Cat"] = df["Benzos_Cat"].astype(int)
+    df["Coke_Cat"] = df["Coke_Cat"].astype(int)
+    df["Ecstasy_Cat"] = df["Ecstasy_Cat"].astype(int)
+    df["Legalh_Cat"] = df["Legalh_Cat"].astype(int)
+    df["LSD_Cat"] = df["LSD_Cat"].astype(int)
+    df["Shrooms_Cat"] = df["Shrooms_Cat"].astype(int) 
+
+    df["Amyl_Cat"] = df["Amyl_Cat"].astype(int)   
+    df["Crack_Cat"] = df["Crack_Cat"].astype(int)
+    df["Heroin_Cat"] = df["Heroin_Cat"].astype(int)
+    df["Ketamine_Cat"] = df["Ketamine_Cat"].astype(int)
+    df["Meth_Cat"] = df["Meth_Cat"].astype(int)
+    df["VSA_Cat"] = df["VSA_Cat"].astype(int)
+
+    return df
 
 
-
-def drug_group_3(df:pd.DataFrame)-> pd.DataFrame:
-    df_group3_drugs = df.drop(["Alcohol_Cat", "Amphet_Cat", "Benzos_Cat", "Caff_Cat", "Cannabis_Cat", "Choc_Cat", "Coke_Cat",
-                        "Ecstasy_Cat", "Legalh_Cat", "LSD_Cat", "Shrooms_Cat", "Nicotine_Cat", "Alcohol", "Amphet", "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke", "Crack",
-                        "Ecstasy", "Heroin", "Ketamine", "Legalh", "LSD", "Meth", "Shrooms", "Nicotine", "VSA"], axis=1)  
-
-    df_group3_drugs["Amyl_Cat"] = df_group3_drugs["Amyl_Cat"].astype(int)
-    df_group3_drugs["Crack_Cat"] = df_group3_drugs["Crack_Cat"].astype(int)
-    df_group3_drugs["Heroin_Cat"] = df_group3_drugs["Heroin_Cat"].astype(int)
-    df_group3_drugs["Ketamine_Cat"] = df_group3_drugs["Ketamine_Cat"].astype(int)
-    df_group3_drugs["Meth_Cat"] = df_group3_drugs["Meth_Cat"].astype(int)
-    df_group3_drugs["VSA_Cat"] = df_group3_drugs["VSA_Cat"].astype(int)
-
-
-
-def drug_groups_join(df:pd.DataFrame)-> pd.DataFrame:
-    df['Group1_drugs']= df_group1_drugs["Alcohol_Cat"] + df_group1_drugs["Caff_Cat"] + df_group1_drugs["Cannabis_Cat"] + df_group1_drugs["Choc_Cat"] + df_group1_drugs["Nicotine_Cat"]
+def drug_groups_join(df):
+    df['Group1_drugs']= df["Alcohol_Cat"] + df["Caff_Cat"] + df["Cannabis_Cat"] + df["Choc_Cat"] + df["Nicotine_Cat"]
     df['Group1_drugs'] = df['Group1_drugs'].apply(lambda x: 1 if x > 0 else 0)
 
-    df['Group2_drugs']= df_group2_drugs["Amphet_Cat"] + df_group2_drugs["Benzos_Cat"] + df_group2_drugs["Coke_Cat"] + df_group2_drugs["Ecstasy_Cat"] + df_group2_drugs["Legalh_Cat"] + df_group2_drugs["LSD_Cat"]+ df_group2_drugs["Shrooms_Cat"]
+    df['Group2_drugs']= df["Amphet_Cat"] + df["Benzos_Cat"] + df["Coke_Cat"] + df["Ecstasy_Cat"] + df["Legalh_Cat"] + df["LSD_Cat"]+ df["Shrooms_Cat"]
     df['Group2_drugs'] = df['Group2_drugs'].apply(lambda x: 1 if x > 0 else 0)
 
-    df['Group3_drugs']= df_group3_drugs["Amyl_Cat"] + df_group3_drugs["Crack_Cat"] + df_group3_drugs["Heroin_Cat"] + df_group3_drugs["Ketamine_Cat"] + df_group3_drugs["Meth_Cat"] + df_group3_drugs["VSA_Cat"]
+    df['Group3_drugs']= df["Amyl_Cat"] + df["Crack_Cat"] + df["Heroin_Cat"] + df["Ketamine_Cat"] + df["Meth_Cat"] + df["VSA_Cat"]
     df['Group3_drugs'] = df['Group3_drugs'].apply(lambda x: 1 if x > 0 else 0)  
 
     df.drop(["Alcohol", "Amphet", "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke", "Crack","Ecstasy", "Heroin", "Ketamine",
           "Legalh", "LSD", "Meth", "Shrooms", "Nicotine", "VSA", "Alcohol_Cat", "Amphet_Cat", "Amyl_Cat", "Benzos_Cat", "Caff_Cat",
           "Cannabis_Cat", "Choc_Cat", "Coke_Cat", "Crack_Cat","Ecstasy_Cat", "Heroin_Cat", "Ketamine_Cat", "Legalh_Cat", "LSD_Cat", 
           "Meth_Cat", "Shrooms_Cat", "Nicotine_Cat", "VSA_Cat"], axis=1, inplace=True)
+    
+    return df
--- train.py
+++ train.py
@@ -1,15 +1,66 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVC
+from sklearn.svm import SVC
+from sklearn.metrics import fbeta_score, accuracy_score
+from imblearn.over_sampling import RandomOverSampler
 import pickle
 from imblearn.over_sampling import RandomOverSampler
 import warnings
 warnings.filterwarnings('ignore')
 
-from feature_engineering import drop_semer, use_non_use, drug_group_1, drug_group_2, drug_group_3, drug_groups_join
+from feature_engineering import drop_semer, use_non_use, make_int, drug_groups_join
 
 #Import Data
 df = pd.read_excel('Data/drug_consumption.xls')
+print("Read in Data")
 
-print("Read dataframe")
+#Adjust dataset to business problem
+df=drop_semer(df)
+df=use_non_use(df)
+df=make_int(df)
+df=drug_groups_join(df)
+print ("Adjusted dataset")
+
+#Split Data into X and Y
+X = df[['Age', 'Gender', 'Education', 'Country', 'Ethnicity', 'Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore', 'Impulsive', 'SS']]
+y = df['Group2_drugs']
+print("Saved X and y")
+
+#Train-Test-Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0, stratify = y) 
+
+print("Saving test data in the data folder")
+X_test.to_csv("data_model/X_test.csv", index=False)
+y_test.to_csv("data_model/y_test.csv", index=False)
+
+# Define oversampling strategy
+oversample = RandomOverSampler(sampling_strategy='minority')
+X_train, y_train = oversample.fit_resample(X_train, y_train)
+
+print ("Training set has {} samples.".format(X_train.shape[0]))
+print ("Testing set has {} samples.".format(X_test.shape[0]))
+
+# Training the model
+clf = SVC(random_state = 420)
+learner = clf.fit(X_train, y_train)
+
+# Train set
+predictions_train = learner.predict(X_train)
+acc_train = accuracy_score(y_train, predictions_train)
+f_train = fbeta_score(y_train,predictions_train,beta=0.5)
+
+#Test set
+predictions_test = learner.predict(X_test)
+acc_test = accuracy_score(y_test, predictions_test)
+f_test = fbeta_score(y_test,predictions_test,beta=0.5)
+
+print (f'Accuracy on trainig data: {acc_train}')
+print (f'Accuracy on testing data: {acc_test}')
+print (f'F_beta_score on trainig data: {f_train}')
+print (f'F_beta_score on test data: {f_test}')
+
+#saving the model
+print("Saving model in the model folder")
+filename = 'models/SVC.sav'
+pickle.dump(learner, open(filename, 'wb'))
